9c9bed7d1229c58c75f34e77e88f912b72294e0b
GGhiaroni <gghiaronitiziano@gmail.com>
2026-09-08 18:46:01 -0300

    chore: estrutura inicial com microservices de clientes e produtos

---
+++ microservice-clientes/src/test/java/gabriel/tiziano/microservice_clientes/MicroserviceClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.tiziano.microservice_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservice-produtos/src/test/java/gabriel/tiziano/microservice_produtos/MicroserviceProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.tiziano.microservice_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
