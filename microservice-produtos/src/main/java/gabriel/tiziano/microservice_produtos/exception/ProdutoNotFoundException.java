42184ac1ee1126727ce5a25dc770f13f32e6bddb
GGhiaroni <gghiaronitiziano@gmail.com>
2026-09-12 16:33:41 -0300

    feat: implementa API REST de produtos com camadas e tratamento de erros
    - Entity Produto mapeada para a tabela produtos (JPA)
    - ProdutoRepository (Spring Data JPA)
    - DTOs de request e response (records) com Bean Validation
    - ProdutoMapper para conversão entity <-> DTO
    - Service com regras e fronteira transacional (readOnly nas leituras)
    - Controller REST: listar, buscar por id e cadastrar
    - Tratamento de erro centralizado (GlobalExceptionHandler + ApiError)
    - Exceção de domínio ProdutoNotFoundException (404)

---
+++ microservice-produtos/src/main/java/gabriel/tiziano/microservice_produtos/exception/ProdutoNotFoundException.java
@@ -0,0 +1,11 @@
+package gabriel.tiziano.microservice_produtos.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProdutoNotFoundException extends RuntimeException {
+    public ProdutoNotFoundException(Long codigo) {
+        super("Produto com código " + codigo + " não encontrado.");
+    }
+}
